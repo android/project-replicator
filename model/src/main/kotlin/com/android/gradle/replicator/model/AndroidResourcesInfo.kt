22c52779d337ab30ccc9df7675f442390bff66f3
Jean-Luc Coelho <jeanlucncoelho_1@hotmail.com>
2021-04-12 16:47:37 -0700

    Add resource count to plugin and project generator
    Test: None

---
+++ model/src/main/kotlin/com/android/gradle/replicator/model/AndroidResourcesInfo.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.gradle.replicator.model
+
+
+data class AndroidResourceExtensions(
+        val extensions: MutableMap<String, Int>
+)
+data class AndroidResourceQualifiers(
+        val qualifiers: MutableMap<String, AndroidResourceExtensions>
+)
+data class AndroidResourceFolders(
+        val folders: MutableMap<String, AndroidResourceQualifiers>
+)
+
+/**
+ * Information about the number of android resource files in a [ModuleInfo]
+ * Divided by folder name, qualifiers and  such as mipmap-hidpi
+ */
+interface AndroidResourcesInfo {
+    val fileCount: AndroidResourceFolders
+}
---
+++ model/src/main/kotlin/com/android/gradle/replicator/model/internal/DefaultAndroidResourcesInfo.kt
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.gradle.replicator.model.internal
+
+import com.android.gradle.replicator.model.AndroidResourceExtensions
+import com.android.gradle.replicator.model.AndroidResourceFolders
+import com.android.gradle.replicator.model.AndroidResourceQualifiers
+import com.android.gradle.replicator.model.AndroidResourcesInfo
+import com.google.gson.TypeAdapter
+import com.google.gson.stream.JsonReader
+import com.google.gson.stream.JsonWriter
+
+val ANDROID_RESOURCE_FOLDERS = mapOf(
+        "animator" to listOf(".xml"),
+        "anim" to listOf(".xml"),
+        "color" to listOf(".xml"),
+        "drawable" to listOf(".xml", ".png", ".9.png", ".jpg", ".gif"),
+        "mipmap" to listOf(".xml", ".png", ".9.png", ".jpg", ".gif", ".webp"),
+        "layout" to listOf(".xml"),
+        "menu" to listOf(".xml"),
+        "raw" to listOf("*"),
+        "values" to listOf(".xml"),
+        "xml" to listOf(".xml"),
+        "font" to listOf(".ttf", ".otf", ".ttc")
+)
+
+data class DefaultAndroidResourcesInfo(
+        override val fileCount: AndroidResourceFolders
+) : AndroidResourcesInfo
+
+/* Resource division is done in the following way:
+ * fname-mod1, fname, fname-mod2 are resource folders. This becomes:
+ * "resources": {
+ *     "fname" : {
+ *         "":  {
+ *             ".xml": 2
+ *         },
+ *         "mod1": {
+ *             ".xml": 3
+ *         },
+ *         "mod2": {
+ *             ".xml": 2,
+ *             ".png": 1
+ *         }
+ *     },
+ * ...
+ * }
+ */
+class AndroidResourcesAdapter: TypeAdapter<AndroidResourcesInfo>() {
+    override fun write(output: JsonWriter, value: AndroidResourcesInfo) {
+        output.beginObject()
+        for (folder in value.fileCount.folders) {
+            val folderObject = output.name(folder.key).beginObject()
+            for (qualifier in folder.value.qualifiers) {
+                val qualifierObject = folderObject.name(qualifier.key).beginObject()
+                for (extension in qualifier.value.extensions) {
+                    qualifierObject.name(extension.key).value(extension.value)
+                }
+                qualifierObject.endObject()
+            }
+            folderObject.endObject()
+        }
+        output.endObject()
+    }
+
+    override fun read(input: JsonReader): AndroidResourcesInfo {
+        val fileCount = AndroidResourceFolders(mutableMapOf())
+
+        // Read folder properties
+        input.readObjectProperties { folderName ->
+            fileCount.folders[folderName] = AndroidResourceQualifiers(mutableMapOf())
+            // Read modifier properties
+            this.readObjectProperties { qualifierName ->
+                fileCount.folders[folderName]!!.qualifiers[qualifierName] = AndroidResourceExtensions(mutableMapOf())
+                this.readObjectProperties { extensionName ->
+                    fileCount.folders[folderName]!!.qualifiers[qualifierName]!!.extensions[extensionName] = nextInt()
+                }
+            }
+        }
+
+        return DefaultAndroidResourcesInfo(fileCount)
+    }
+}
